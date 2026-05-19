4b8d37c89b588ad2fd7ef42974311f4a1acf73ff
Emily95Yi <165650493+Emily95Yi@users.noreply.github.com>
2026-05-19 19:12:04 +0900

    Set Vite build target to esnext

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
+  esbuild: {
+    target: "esnext",
+  },
   server: {
     host: "0.0.0.0",
     port: 5173,
